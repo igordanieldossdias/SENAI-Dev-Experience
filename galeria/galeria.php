0f65a2c1077037ec859dcea9e513b4b8985a6aa3
igordanieldossdias <igor.danielesu@gmail.com>
2023-11-17 13:30:25 -0300

    carossel: Galeria e index

--- galeria/galeria.php
+++ galeria/galeria.php
@@ -3,8 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SENAI Dev Experience</title>
-    <link rel="stylesheet" href="index_css.css">
+    <title>Galeria Dev Experience</title>
+    <link rel="stylesheet" href="galeria_css.css">
     </head>
 <body> 
          
@@ -15,25 +15,29 @@
             <input type="radio" name="radio-bt" id="radio1"> 
             <input type="radio" name="radio-bt" id="radio2">
             <input type="radio" name="radio-bt" id="radio3">
+            <input type="radio" name="radio-bt" id="radio4">
             <!--Fim radio bolinhas-->  
             
             <!--imagens-->
             <div class="slide um">
-                <img src="Banner - DEV Exp1.png"/>
+                <img src="https://i.pinimg.com/736x/88/4a/53/884a538e964abce618af4fa6f075190b.jpg" alt=""/>
             </div>
             <div class="slide">
-                <img src="Banner - DEV Exp2.png" alt=""/>
+                <img src="https://i.pinimg.com/originals/f3/41/b2/f341b2024bb0b14365296cc862b60299.jpg" alt=""/>
             </div>
             <div class="slide">
-                <img src="Banner - DEV Exp3.png" alt=""/>
+                <img src="https://i.pinimg.com/originals/9c/b5/72/9cb5721e9e13eca0e3c1275d1bd9d981.jpg" alt=""/>
             </div>
-
+            <div class="slide">
+                <img src="https://i.pinimg.com/1200x/6e/d4/40/6ed440eda1a9fb045f51e3046051156b.jpg" alt=""/>
+            </div>
             <!--fim imagens-->
 
             <div class="navegacao-auto">
                 <div class="auto1"></div>
                 <div class="auto2"></div>
                 <div class="auto3"></div>
+                <div class="auto4"></div>
 
             </div>
             
@@ -43,11 +47,11 @@
             <label for="radio1" class="manual-bt"></label>
             <label for="radio2" class="manual-bt"></label>
             <label for="radio3" class="manual-bt"></label>
-
+            <label for="radio4" class="manual-bt"></label>
         </div>
     </div>
 
-    <script src="index_js.js"></script>
+    <script src="galeria_js.js"></script>
 
 </body>
 </html>
---
+++ galeria/galeria_js.js
@@ -0,0 +1,17 @@
+let count = 1
+document.getElementById("radio1").checked = true;
+
+setInterval ( function(){
+    proximg();
+}, 2000)
+
+function proximg(){
+    count ++;
+    if(count>4){
+        count= 1;
+    }
+
+    document.getElementById("radio"+count).checked = true;
+
+}
+
